fcc19a98eb90864c19919f64d11d48e8e81b70ba
alanteixido <alan.teixido.7e7@itb.cat>
2024-01-26 09:09:19 +0100

    2 exemples ClassCar(Exemple).kt & Fun&Ckass(Exemple).kt

--- src/main/kotlin/ClassCar(Exemple).kt
+++ src/main/kotlin/ClassCar(Exemple).kt
@@ -20,3 +20,31 @@ fun main() {
     println(c1.brand)
     println(c2.brand)
 }
+class Car2 {
+    var brand2 = ""
+    var model2 = ""
+    var year2 = 0
+}
+
+fun main2() {
+    val c12 = Car()
+    c12.brand = "Ford"
+    c12.model = "Mustang"
+    c12.year = 1969
+
+    println(c12.brand)
+    println(c12.model)
+    println(c12.year)
+}
+
+class Car3 (var brand3: String, var model3: String, var year3: Int)
+
+fun main3() {
+    val c13 = Car3("Ford", "Mustang", 1969)
+    val c23 = Car3("BMW", "X5", 1999)
+    val c33 = Car3("Tesla", "Model S", 2020)
+
+    println(c13.brand3)
+    println(c23.brand3)
+    println(c33.brand3)
+}
