dda85d9bbc0975c4ce10e66c5b48aa2f336fb271
alanteixido <alan.teixido.7e7@itb.cat>
2024-01-29 13:44:35 +0100

    Exercici 3

---
+++ src/main/kotlin/Activitat3/OO1.kt
@@ -0,0 +1,14 @@
+package org.example.Activitat3
+
+/*
+fun main(args: Array<String>) {
+    val johndoe: Persona = Persona()
+    johndoe.presentacio()
+    val manuel: Persona = Persona("Manuel", "Ruiz González")
+    manuel.presentacio()
+    val eva: Persona = Persona("Eva", "Castillo Real", 31)
+    eva.presentacio()
+    val ana: Persona = Persona("Ana", "Díaz Marco", 25, 1.65, 53)
+    ana.presentacio()
+}
+*/
