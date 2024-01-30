c2cf42db3036f6687ae4fd4055c9f37c26d9a8f4
alanteixido <alan.teixido.7e7@itb.cat>
2024-01-30 11:39:18 +0100

    EX 2 de Activitats_UF4_A4 finalitzats

--- src/main/kotlin/Activitats_UF4_A4/EXClassePersona.kt
+++ src/main/kotlin/Activitats_UF4_A4/EXClassePersona.kt
@@ -49,35 +49,3 @@ class EXClassePersona () {
         return "Persona(dni='$dni', nom='$nom', cognoms='$cognoms', edat=$edat, estatCivil=$estatCivil)"
     }
 }
-
-class Cercle() {
-    var radi: Float = 0f
-    var color: String = "blanc"
-
-    init {
-        radi = 10f
-        color = "blanc"
-    }
-
-    constructor(radi: Float) : this() {
-        this.radi = radi
-    }
-
-    constructor(radi: Float, color: String) : this() {
-        this.radi = radi
-        this.color = color
-    }
-
-    fun perimetre(): Float {
-        return 2 * Math.PI * radi
-    }
-
-    fun area() : Float {
-        return Math.PI * radi * radi
-    }
-
-    override fun toString(): String {
-        return "Cercle(radi: $radi, color: $color)"
-    }
-
-}
--- src/main/kotlin/Activitats_UF4_A4/main.kt
+++ src/main/kotlin/Activitats_UF4_A4/main.kt
@@ -1,17 +1,69 @@
 package org.example.Activitats_UF4_A4
-import org.example.Activitat3.Persona
 
 fun main() {
-
+    mostrarMenuPersona()
     mostrarMenuCercle()
-
-
-
-
 }
 
-fun mostrarMenuCercle () : String{
+fun mostrarMenuCercle() {
+    var cercle: Cercle? = null
+    println("\nARA EL CERCLE\n")
 
+    while (true) {
+        println("\nMenu:")
+        println("1.- Crear un cercle d'un radi  i d'un color ")
+        println("2.- Crear un cercle d'un radi ")
+        println("3.- Mostrar últim cercle creat")
+        println("4.- Mostrar perímetre últim cercle creat")
+        println("5.- Mostrar àrea últim cercle creat")
+        println("6.- Sortir")
+
+        print("Escull una opció: ")
+        val opcio = readLine()
+
+        when (opcio) {
+            "1" -> {
+                println("Introdueix el radi del cercle")
+                val radi = readLine()?.toFloat() ?: 0f
+
+                println("Introdueix el color del cercle")
+                val color = readLine() ?: ""
+                cercle = Cercle(radi, color)
+                println("Cercle de: $cercle")
+            }
+
+            "2" -> {
+                println("Introdueix el radi del cercle")
+                val radi = readLine()?.toFloat() ?: 0f
+                cercle = Cercle(radi)
+                println("Cercle de: $cercle")
+            }
+
+            "3" -> {
+                println("Ultim cercle creat ${cercle.toString()}")
+            }
+
+            "4" -> {
+                println("Perímetre del ultim cercle creat: ${cercle?.perimetre()}")
+            }
+
+            "5" -> {
+                println("Area ultim cercle ${cercle?.area()}")
+            }
+
+            "6" -> {
+                println("Adeu Andreu")
+                break
+            }
+
+            else -> {
+                println("Opcio incorrecte... :(")
+            }
+        }
+    }
+}
+
+fun mostrarMenuPersona (){
     println("\n ARA LA PERSONA \n")
 
     val persona1 = EXClassePersona()
@@ -30,64 +82,4 @@ fun mostrarMenuCercle () : String{
     println(persona1)
     println(persona2)
     println(persona3)
-
-}
-
-fun mostrarMenuPersona () :String{
-    println("\n ARA EL CERCLE \n")
-
-    while (true){
-        println("\nMenu:")
-        println("1.- Crear un cercle d'un radi  i d'un color ")
-        println("2.- Crear un cercle d'un radi ")
-        println("3.- Mostrar últim cercle creat")
-        println("4.- Mostrar perímetre últim cercle creat")
-        println("5.- Mostrar àrea últim cercle creat")
-        println("6.- Sortir")
-
-        print("Escull una opció: ")
-        val opcio = readln()
-
-    when (opcio){
-        "1"-> {
-            println("Introdueix el radi del cercle")
-            val radi = readln().toFloat()
-
-            println("Introdueix el color del cercle")
-            val color = readln()
-            val cercle= Cercle(radi, color)
-            println("Cercle de: $cercle")
-        }
-
-        "2"->{
-            println("Introdueix el radi del cercle")
-            var radi = readln().toFloat()
-            val cercle = Cercle(radi)
-            println("Cercle de: $cercle")
-        }
-
-        "3"->{
-            println("Ultim cercle creat $cercle")
-
-
-        }
-
-        "4"->{
-            println("Parametres del ultim cercle creat: $")
-        }
-
-        "5"->{
-
-        }
-        "6"->{
-            println("Adeu Andreu")
-            break
-        }
-        else ->{
-            println("Opcio incorrecte... :(")
-        }
-
-
-     }
-    }
 }
