a0a5f191e86a34fe0f89f16e064972a234f00dff
alanteixido <alan.teixido.7e7@itb.cat>
2024-01-31 11:18:19 +0100

    5 acabat.

--- src/main/kotlin/Activitats_UF4_A5/models/Beguda.kt
+++ src/main/kotlin/Activitats_UF4_A5/models/Beguda.kt
@@ -11,6 +11,29 @@ class Beguda {
         this.teIncrement = teIncrement
     }
 
+    fun getNom(): String? {
+        return nom
+    }
+
+    fun setNom(pNom: String){
+        this.nom = pNom
+    }
+
+    fun getPreu(): Double? {
+        return preu
+    }
+
+    fun setPreu(pPreu: Double){
+        this.preu = pPreu
+    }
+
+    fun getIncrementSucre(): Boolean? {
+        return teIncrement
+    }
+
+    fun setIncrementSucre(pIncrementSucre: Boolean){
+        this.teIncrement = pIncrementSucre
+    }
     override fun toString(): String {
         return "Beguda (nom = $nom, preu = $preu, teIncrement = $teIncrement)"
     }
--- src/main/kotlin/Activitats_UF4_A5/models/Pastisseria.kt
+++ src/main/kotlin/Activitats_UF4_A5/models/Pastisseria.kt
@@ -13,6 +13,18 @@ class Pastisseria {
         this.pes = pes
         this.calories = calories
     }
+
+    fun getNom(): String? = nom
+    fun setNom(nom: String) { this.nom = nom }
+
+    fun getPreu(): Double? = preu
+    fun setPreu(preu: Double) { this.preu = preu }
+
+    fun getPes(): Double? = pes
+    fun setPes(pes: Double) { this.pes = pes }
+
+    fun getCalories(): Int? = calories
+    fun setCalories(calories: Int) { this.calories = calories }
     override fun toString() :String{
         return "Pasta (nom = $nom, preu = $preu, pes = $pes, calories = $calories"
     }
