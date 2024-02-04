8ca2755a85fee1577f3d1733de994599b7fa0554
alanteixido <alan.teixido.7e7@itb.cat>
2024-02-04 13:53:01 +0100

    Projecte acabat. Petits fallos i kdoc fet.

--- src/main/kotlin/Activitat_8_Electrodomestics/models/Televisio.kt
+++ src/main/kotlin/Activitat_8_Electrodomestics/models/Televisio.kt
@@ -1,14 +1,25 @@
+/**
+ * Classe que representa una televisió, una subclasse d'Electrodomestic.
+ *
+ * @property mida Mida de la televisió en polzades.
+ * @constructor Crea una televisió amb els paràmetres especificats.
+ */
 class Televisio(
     preuBase: Int,
     color: String = "blanc",
-    consum: Int = 0,
+    consum: String = "G",  // Cambiado a String
     pes: Int = 5,
     var mida: Int = 28
 ) : Electrodomestic(preuBase, color, consum, pes) {
+    /**
+     * Calcula el preu final de la televisió, tenint en compte la mida addicional.
+     *
+     * @return Preu final de la televisió.
+     */
     override fun preuFinal(): Int {
         var preuFinal = super.preuFinal()
 
-        // Incremento del precio en función de la medida
+        // Increment del preu en funció de la mida
         when {
             mida in 29..32 -> preuFinal += 50
             mida in 33..42 -> preuFinal += 100
@@ -19,6 +30,11 @@ class Televisio(
         return preuFinal
     }
 
+    /**
+     * Retorna una representació en format de cadena de l'objecte Televisio.
+     *
+     * @return Representació en format de cadena de l'objecte.
+     */
     override fun toString(): String {
         return """
             Televisió:
