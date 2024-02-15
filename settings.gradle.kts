3b5de756d6c0a504fd04f598a9eda554ad4b5f3f
alanteixido <alan.teixido.7e7@itb.cat>
2024-02-15 11:34:56 +0100

    UML carpeta

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -2,4 +2,5 @@ plugins {
     id("org.gradle.toolchains.foojay-resolver-convention") version "0.5.0"
 }
 rootProject.name = "ProgramaccioObjectes"
-
+include("src:main:UML")
+findProject(":src:main:UML")?.name = "UML"
